9ec6d4e53f264a24dea835a12a52d8875cb68ec5
agolautner <lautner.agoston@gmail.com>
2022-07-03 11:48:55 +0200

    removed unnecessary project folders

--- oid-backend/middlewares/errorHandler.js
+++
@@ -1,7 +0,0 @@
-const logger = require('../utils/logger')
-
-exports.errorHandler = (err, req, res, next) => {
-  console.log(err);
-  logger.error(new Error("render error"), err.toString());
-  res.status(500).json("Something went wrong!");
-};
--- oid-frontend/Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM nginx:alpine
-COPY ./build /usr/share/nginx/html
-RUN rm /etc/nginx/conf.d/default.conf
-COPY nginx/nginx.conf /etc/nginx/conf.d
-EXPOSE 80
-CMD ["nginx", "-g", "daemon off;"]
